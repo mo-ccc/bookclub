092f2780ac9c87e674472d1cade0c56a4f8e27ec
mo <css012002@coderacademy.edu.au>
2021-03-12 12:15:47 +1100

    fixed yml

--- rest_api/src/default_settings.py
+++ rest_api/src/default_settings.py
@@ -5,7 +5,7 @@ import os
 dotenv.load_dotenv()
 
 def get_from_env(var_name):
-    value = os.environ.get(var_name)
+    value = os.getenv(var_name)
 
     if not value:
         raise ValueError(f"{var_name} is not set")
@@ -17,7 +17,7 @@ class Config():
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    SERVER_NAME = os.get_from_env("HOSTNAME")
+    SERVER_NAME = get_from_env("HOSTNAME")
 
     JWT_SECRET_KEY = get_from_env('JWT_SECRET_KEY')
     if os.getenv("DB_URL"):
@@ -47,4 +47,4 @@ elif environment == 'testing':
 elif environment == 'production':
     configuration = Production()
 else:
-    raise Exception('FLASK_ENV is not set properly. use development, testing or production')
+    raise ValueError('FLASK_ENV is not set properly. use development, testing or production')
