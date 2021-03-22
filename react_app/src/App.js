a08b18124b54a82db353260a4d0e053b0462a438
mo <css012002@coderacademy.edu.au>
2021-03-22 23:33:19 +1100

    optimized formbase2

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -3,30 +3,24 @@ import {Provider} from 'react-redux'
 import store from './redux/store.js'
 import Main from './Main.js'
 import {BrowserRouter} from 'react-router-dom'
-import FormBase from './components/FormBase'
+import FormBase2 from './components/FormBase2'
 import {useForm} from 'react-hook-form'
 
 const App = () => {
   const useform = useForm()
   const handleSubmit = (data) => {
-    return fetch(`http://${process.env.REACT_APP_HOST}/`, {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    }).then(response => {
-      if (response.ok) {
-        window.location.replace(`http://${data.domain_name}.${process.env.REACT_APP_DOMAIN}/`)
-      }else{
-        alert("Could not successfully create subdomain")
-      }
-    }).catch(error => alert(error))
+    console.log(data)
   }
 
-  const former = [
-    {"name": "domain_name", "label": "domain name", "placeholder": "enter a name for a new domain", "inputType": "text", "validation": {"maxLength": 10} },
+  const formFields = [
+    {name: "domain.domain_name", label: "domain name", placeholder: "enter a name for a new domain", inputType: "text", validation: {maxLength: 10, required: true} },
+
+    {name: "user.name", label: "name", placeholder: "enter a name for the root user", inputType: "test", validation: {required: true, pattern: {value: /^[a-zA-Z ]*$/, message: "name must only contain letters and white spaces"}}},
+
+    {name: "user.email", label: "email", placeholder: "enter an email for the root user", inputType: "email", validation: {required: true}},
+
+    {name: "user.password", label: "password", placeholder: "enter a password for the root user", inputType: "password", validation: {required: true, minLength: 6}}
+    
   ]
 
   if (window.location.hostname === process.env.REACT_APP_DOMAIN){
@@ -45,7 +39,7 @@ const App = () => {
             <div className="card text-black-50 bg-light p-4 m-4">
               <h6>Create a new subdomain</h6>
               
-              <FormBase fields={former} useForm={useform} onSubmit={handleSubmit}/>
+              <FormBase2 fields={formFields} useForm={useform} onSubmit={handleSubmit}/>
 
               <h6 className="mt-5">or visit the demo subdomain: <a href={`http://demo.${process.env.REACT_APP_DOMAIN}`}>http://demo.{process.env.REACT_APP_DOMAIN}</a></h6>
             </div>
--- react_app/src/components/FormBase2.js
+++ react_app/src/components/FormBase2.js
@@ -2,17 +2,25 @@ import React from 'react'
 import Select from 'react-select'
 import {Controller} from 'react-hook-form'
 import times from '../statics/json/times.json'
+import _ from 'lodash';
 
 const FormBase = ({defaultData, onSubmit, useForm, fields}) => {
   const {register, handleSubmit, control, errors} = useForm
   const boolOptions = [
     {value:"true", label:"true" }, {value:"false", label:"false" }
   ]
-  // {label, name, inputType, validation, newline, options}
-
+  
   return (
     <form onSubmit={handleSubmit(onSubmit)} >
       {fields.map((item, i) => {
+        const errorMessages = {
+          "maxLength": `length must be less than ${item.validation?.maxLength}`,
+          "minLength": `length must be greater than ${item.validation?.minLength}`,
+          "required": "field is required"
+        }
+        console.log(errors)
+      
+
         switch (item.inputType) {
           case "select": return (
             <Controller as={Select} name={item.name} options={item.options} defaultValue={defaultData?.[item.name]} control={control} ref={register(item.validation)}/>
@@ -25,9 +33,9 @@ const FormBase = ({defaultData, onSubmit, useForm, fields}) => {
           default: return (
             <div className="form-group" key={i}>
               <label>{item.label}</label>
-              <input className="form-control" name={item.name} type={item.inputType} defaultValue={defaultData?.[item.name]} ref={register(item.validation)} />
-              {errors[item] &&
-                <small className="form-text text-muted">{errors[item]}</small>
+              <input className="form-control" name={item.name} placeholder={item.placeholder} type={item.inputType} defaultValue={defaultData?.[item.name]} ref={register(item.validation)} />
+              {_.get(errors, item.name) &&
+                <small className="form-text text-muted">{_.get(errors, item.name).message || errorMessages[_.get(errors, item.name).type]}</small>
               }
             </div>
           )
