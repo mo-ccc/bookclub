9edffdbe1576426a6812c2e355da5005b76befe3
mo <css012002@coderacademy.edu.au>
2021-03-23 21:56:06 +1100

    further refined formbase2

--- react_app/src/components/FormBase2.js
+++ react_app/src/components/FormBase2.js
@@ -1,20 +1,14 @@
 import React from 'react'
-import {Controller} from 'react-hook-form'
-import times from '../statics/json/times.json'
 import _ from 'lodash';
 
 const FormBase = ({defaultData, onSubmit, useForm, fields}) => {
   const {register, handleSubmit, control, errors} = useForm
-  const boolOptions = [
-    {value:"true", label:"true" }, {value:"false", label:"false" }
-  ]
 
   const renderInput = {
     "select": (item) => (
       <div>
         <select className="form-control" name={item.name} ref={register(item.validation)} defaultValue={defaultData?.[item.name]}>
-          <option key="true" value="true">true</option>
-          <option key="false" value="false">false</option>
+          {item.options.map((option, i) => <option key={option} value={option}>{option}</option>)}
         </select>
       </div>
     ),
@@ -25,47 +19,34 @@ const FormBase = ({defaultData, onSubmit, useForm, fields}) => {
           <option key="false" value="false">false</option>
         </select>
       </div>
+    ),
+    "flex": (item) => (
+      <div>
+        {item.fields.map((item, i) => {
+          return renderer(item, i)
+        })}
+      </div>
     )
   }
   
+  const renderer = (item, i) => (
+    <div className="form-group" key={item.name}>
+      <label>{item.label}</label>
+      {_.has(errors, item.name) &&
+        <small className="form-text text-muted">{_.get(errors, item.name).message}</small>
+      }
+      {item.inputType in renderInput ?
+        renderInput[item.inputType](item)
+          :
+        <input className="form-control" name={item.name} placeholder={item.placeholder} type={item.inputType} defaultValue={defaultData?.[item.name]} ref={register(item.validation)} />
+      }
+    </div>
+  )
+
   return (
     <form onSubmit={handleSubmit(onSubmit)} >
       {fields.map((item, i) => {
-        
-        switch (item.inputType) {
-          case "select": return (
-            <div>
-              <select className="form-control" name={item.name} ref={register(item.validation)} defaultValue={defaultData?.[item.name]}>
-                <option key="true" value="true">true</option>
-                <option key="false" value="false">false</option>
-              </select>
-            </div>
-          )
-        
-          case "bool": return (
-            <div className="form-group">
-              <label>{item.label}</label>
-              {_.has(errors, item.name) &&
-                <small className="form-text text-muted">{_.get(errors, item.name).message}</small>
-              }
-              <select className="form-control" name={item.name} ref={register(item.validation)} defaultValue={defaultData?.[item.name]}>
-                <option key="true" value="true">true</option>
-                <option key="false" value="false">false</option>
-              </select>
-            </div>
-          )
-
-          default: return (
-            <div className="form-group" key={i}>
-              <label>{item.label}</label>
-              {_.has(errors, item.name) &&
-                <small className="form-text text-muted">{_.get(errors, item.name).message}</small>
-              }
-              <input className="form-control" name={item.name} placeholder={item.placeholder} type={item.inputType} defaultValue={defaultData?.[item.name]} ref={register(item.validation)} />
-              
-            </div>
-          )
-        }
+        return renderer(item, i)
       })}
       <input type="submit" className="btn btn-primary"/>
     </form>
